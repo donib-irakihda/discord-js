d3e0bdd1df22af7203fe8b32725719de1f63b38b
donib-irakihda <donib.irakihda@gmail.com>
2023-06-06 17:56:39 +0545

    options and choices

--- src/index.js
+++ src/index.js
@@ -40,4 +40,15 @@ client.on("interactionCreate", (interaction) => {
   }
 });
 
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "add") {
+    const num1 = interaction.options.get("first-number").value;
+    const num2 = interaction.options.get("second-number").value;
+    console.log(num1);
+    interaction.reply(`the sum is ${num1 + num2}`);
+  }
+});
+
 client.login(process.env.TOKEN);
